bdc50858875c40e45d54bdb84ad46b621fac37c4
Matthias J. Kannwischer <matthias@kannwischer.eu>
2021-04-15 17:16:09 +0800

    patch hal and kernel module to work on the Cortex-A7 as well

--- common/hal-cortexa.c
+++ common/hal-cortexa.c
@@ -12,12 +12,25 @@ void hal_send_str(const char* in)
   printf("%s\n", in);
 }
 
+#ifdef __aarch64__
 uint64_t hal_get_time()
 {
   uint64_t t;
   asm volatile("mrs %0, PMCCNTR_EL0":"=r"(t));
   return t;
 }
+#else
+
+uint64_t hal_get_time()
+{
+  // TODO: this is actually a 32-bit counter, so it won't work for very long running schemes
+  //       need to figure out a way to get a 64-bit cycle counter
+  unsigned cc;
+  asm volatile("mrc p15, 0, %0, c9, c13, 0" : "=r"(cc));
+  return cc;
+}
+#endif
+
 
 size_t hal_get_stack_size(void)
 {
--- enable_ccr/enable_ccr.c
+++ enable_ccr/enable_ccr.c
@@ -3,9 +3,15 @@
 
 void enable_ccr(void *info)
 {
+   #ifdef __aarch64__
    asm volatile("MSR PMUSERENR_EL0, %0" ::"r"(1));
    asm volatile("MSR PMCR_EL0, %0" ::"r"(1));
    asm volatile("MSR PMCNTENSET_EL0, %0" ::"r"(0x80000000));
+   #else
+   asm volatile("mcr p15, 0, %0, c9, c14, 0" :: "r"(1));
+   asm volatile("mcr p15, 0, %0, c9, c12, 0" :: "r"(1 | 16));
+   asm volatile("mcr p15, 0, %0, c9, c12, 1" :: "r"(0x8000000f));
+   #endif
 }
 
 int init_module(void)
